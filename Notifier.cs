00a2d5aefaee3bafbe33bb90fe1c026a7a1aa067
Saverio Delpriori <saverio.delpriori@uniurb.it>
2021-01-24 12:46:14 +0100

    Initial commit

---
+++ Notifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProvaScrittaPMO_25_01_2021
+{
+    public class Notifier
+    {
+
+        private static Notifier instance;
+
+        public static Notifier GetInstance()
+        {
+            instance = new Notifier();
+            return instance;
+        }
+
+        private Notifier() { }
+
+
+        public void FireEvent()
+        {
+            wrongInput(this, null);
+        }
+
+        public event EventHandler wrongInput;
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProvaScrittaPMO_25_01_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var notifier = Notifier.GetInstance();
+            notifier.wrongInput += Notifier_wrongInput;
+
+            while (true)
+            {
+                Console.WriteLine("Please type a number:");
+
+                var input = Console.ReadLine();
+                var intInput = int.Parse(input);
+                if (intInput == 3)
+                    Notifier.GetInstance().FireEvent();
+                    Console.WriteLine("Input is 3!");
+            }
+        }
+
+        private static void Notifier_wrongInput(object sender, EventArgs e)
+        {
+            Console.WriteLine("Wrong input");
+        }
+    }
+}
